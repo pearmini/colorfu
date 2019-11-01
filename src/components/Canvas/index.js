fa5e15dd487cd7250002fd5eb5872d231455854c
pearmini <subairui@icloud.com>
2019-11-01 19:20:39 +0800

    完成了基本的功能

--- src/components/Canvas/index.js
+++ src/components/Canvas/index.js
@@ -1,31 +1,32 @@
-import React, {
-    useEffect
-} from 'react'
-import {
-    awesome
-} from '../../lib/awesome-poster'
+import React, { useEffect } from "react";
 
-export default function ({params}) {
-    let canvas = null;
-    const poster = awesome.poster();
 
-    useEffect(() => {
-        if (canvas === null) {
-            canvas = document.getElementById('app');
-            poster.canvas(canvas);
-        }
+let canvas = null;
+export default function({ params, windowSize, handleChangeMode, poster }) {
+  useEffect(() => {
+    if (canvas === null) {
+      canvas = document.getElementById("app");
+      poster.canvas(canvas);
+    }
 
-        poster.width(params.width)
-            .height(params.height)
-            .imageURL(params.imageURL)
-            .fontSize(params.fontSize)
-            .fontFamily(params.fontFamily)
-            .title(params.title)
-            .content(params.content)
-            .textColor(params.textColor)
-            .on('end', console.log);
+    poster
+      .width(windowSize.width)
+      .height(windowSize.height)
+      .imageURL(params.imageURL)
+      .fontSize(params.fontSize)
+      .fontFamily(params.fontFamily)
+      .title(params.title)
+      .content(params.content)
+      .textColor(params.textColor)
+      .layout(params.layout)
+      .fontURL(params.fontURL)
+      .on("end", console.log);
 
-        poster.start();
-    })
-    return <canvas id = "app" > </canvas>
+    poster.draw();
+  });
+  return (
+    <div onClick={handleChangeMode}>
+      <canvas id="app"></canvas>
+    </div>
+  );
 }
