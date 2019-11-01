130a3b10553f58b8f8d5e4badad3e6c73215f1f2
pearmini <subairui@icloud.com>
2019-11-01 19:58:39 +0800

    feat: update site

---
+++ docs/precache-manifest.7e405f71300b0880916e74da325197aa.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87ea300441b53cc13e88012d36a2e909",
+    "url": "./index.html"
+  },
+  {
+    "revision": "36163e9d655c352e1b78",
+    "url": "./static/css/main.11a1eb2e.chunk.css"
+  },
+  {
+    "revision": "c9fd78bc47387de20f3d",
+    "url": "./static/js/2.4ca78c91.chunk.js"
+  },
+  {
+    "revision": "36163e9d655c352e1b78",
+    "url": "./static/js/main.fd84e99f.chunk.js"
+  },
+  {
+    "revision": "4cf22bfa3cb747404641",
+    "url": "./static/js/runtime-main.62125880.js"
+  },
+  {
+    "revision": "9f9ed93520d89731b31390f5e2d29340",
+    "url": "./static/media/example3.9f9ed935.jpeg"
+  }
+]);
