9f864a417f703bc79c2951b5906fa5be835f6621
pearmini <subairui@icloud.com>
2021-03-30 23:09:09 +0800

    feat(components): add screen

---
+++ src/components/Screen.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+.container {
+  width: 100%;
+  height: 100%;
+}
+</style>
