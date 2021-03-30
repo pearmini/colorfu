0d96ee26dcb2145ed2ebf8eee7fa4ba7b127cee6
pearmini <subairui@icloud.com>
2021-03-30 23:10:57 +0800

    feat(home): basic structure

--- src/App.vue
+++ src/App.vue
@@ -26,6 +26,7 @@ html,
 body {
   width: 100%;
   height: 100%;
+  overflow: hidden;
 }
 
 #app {
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,35 +1,56 @@
 <template>
   <div>
-    <h1>Home</h1>
-    <div class="wallpaper"><wallpaper :data="wallpaper" /></div>
+    <h1>Carpe Diem</h1>
+    <p>Use colors to make awesome wallpaper to carpe diem. 🍉</p>
+    <button>Get Started</button>
+    <scale
+      v-for="example in examples"
+      :key="example.type"
+      :data="example.scale"
+    >
+      <screen :type="examples.type">
+        <wallpaper :data="example.data" />
+      </screen>
+    </scale>
   </div>
 </template>
 
 <script>
 import Wallpaper from "../components/Wallpaper.vue";
+import Scale from "../components/Scale.vue";
+import Screen from "../components/Screen.vue";
+
 export default {
   data: () => ({
-    wallpaper: {
-      title: "Carpe Diem",
-      bgColor: "#132743",
-      textColor: "#d7385e",
-      fontSize: 200,
-      fontFamily: "Luckiest Guy",
-    },
+    examples: [
+      {
+        type: "mac",
+        data: {
+          title: "How are you?",
+          bgColor: "#132743",
+          textColor: "#d7385e",
+          fontSize: 280,
+          fontFamily: "Luckiest Guy",
+        },
+        scale: {
+          domain: [0, 200],
+          range: {
+            width: [],
+            height: [],
+            x: [],
+            y: [],
+          },
+        },
+      },
+    ],
   }),
   components: {
     Wallpaper,
+    Scale,
+    Screen,
   },
 };
 </script>
 
 <style>
-.wallpaper {
-  position: absolute;
-  left: 0px;
-  top: 0px;
-  z-index: 10;
-  width: 100%;
-  height: 100%;
-}
 </style>
