04c6fc8df10cec3956362cdaa1f66469561735b5
pearmini <subairui@icloud.com>
2021-03-30 23:08:45 +0800

    feat(components): add scale

---
+++ src/components/Scale.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="container"><slot></slot></div>
+</template>
+
+<script>
+export default {
+  props: {
+    data: {
+      domain: Array,
+      range: {
+        width: Array,
+        height: Array,
+        x: Array,
+        y: Array,
+      },
+    },
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  position: absolute;
+  z-index: 10;
+  left: 0;
+  top: 0;
+  width: 100%;
+  height: 100%;
+}
+</style>
