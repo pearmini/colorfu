0fed659b11e05e49a41728a6caae3476ce37f5c0
Susan <527971893@qq.com>
2021-10-19 22:40:43 +0800

    refactor(wave-pattern): add arc path for wave pattern (#38)
    * refactor(wave-pattern): add arc path for wave pattern
    
    * refactor(wave-pattern): 解决间隙问题

--- src/utils/pattern/wave.js
+++ src/utils/pattern/wave.js
@@ -1,12 +1,62 @@
-export function wave(context, width, height, { backgroundColor, foregroundColor, lineWidth }) {
+export function wave(context, width, height, { backgroundColor, foregroundColor, lineWidth, arcRadius }) {
   context.fillStyle = backgroundColor;
   context.fillRect(0, 0, width, height);
   context.strokeStyle = foregroundColor;
   context.lineWidth = lineWidth;
   context.lineCap = 'square';
-  context.beginPath();
-  context.moveTo(0, height * (3 / 4));
-  context.lineTo(width * (1 / 2), height * (1 / 4));
-  context.lineTo(width, height * (3 / 4));
-  context.stroke();
+  const points = [
+    { x: - width * (1 / 4) , y: height * (3 / 4) },
+    { x: width * (1 / 4), y: height * (1 / 4) },
+    { x: width * (3 / 4), y: height * (3 / 4) },
+    { x: width * (5 / 4), y: height * (1 / 4) },
+  ];
+  const p = getPath(points, arcRadius);
+  const path = new Path2D(p);
+  context.stroke(path);
+}
+
+/**
+ * arc path algorithm from: https://github.com/AlexSugak/react-svg-pathline
+*/
+
+const isCollinear = (p1, p2, p3) => {
+  return (p1.y - p2.y) * (p1.x - p3.x) == (p1.y - p3.y) * (p1.x - p2.x);
+}
+
+const moveTo = (b, a, r) => {
+  const vector = { x: b.x - a.x, y: b.y - a.y };
+  const length = Math.sqrt((vector.x * vector.x) + (vector.y * vector.y));
+  const unitVector = { x: vector.x / length, y: vector.y / length };
+  return { x: a.x + unitVector.x * r, y: a.y + unitVector.y * r };
+}
+
+function getPath(points, r) {
+  const path = points
+    .slice(1)
+    .reduce((acc, p, i, points) => {
+      let next = points[i + 1];
+      let prev = acc[acc.length - 1];
+
+      if (next && !isCollinear(prev.point, p, next)) {
+        let before = moveTo(prev.point, p, r);
+        let after = moveTo(next, p, r);
+        return acc.concat({
+          point: p,
+          s: `L ${before.x} ${before.y} S ${p.x} ${p.y} ${after.x} ${after.y} `
+        })
+      } else {
+        return acc.concat({
+          point: p,
+          s: `L ${p.x} ${p.y} `
+        })
+      }
+    }
+      , [{
+        point: points[0],
+        s: `M ${points[0].x} ${points[0].y} `
+      }])
+    .map(p => p.s)
+    .join('');
+
+  return path;
 }
