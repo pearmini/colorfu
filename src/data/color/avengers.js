74ace2a3ae1e44d617e2464d0e34b3f5c8de347b
pearmini <subairui@icloud.com>
2021-11-07 14:15:07 +0800

    feat(colors): add marvel comics

---
+++ src/data/color/avengers.js
@@ -0,0 +1,45 @@
+export const avengers = {
+  name: "Marvel Comics",
+  colors: [
+    {
+      name: "Scarlet Witch",
+      values: ["#bd3232", "#d98b94"],
+      link: "https://www.marvel.com/characters/scarlet-witch-wanda-maximoff"
+    },
+    {
+      name: "Iron Man",
+      values: ["#b93233", "#eabe4f"],
+      link: "https://www.marvel.com/characters/iron-man-tony-stark"
+    },
+    {
+      name: "Deadpool",
+      values: ["#1e1717", "#aa282e"],
+      link: "https://www.marvel.com/characters/deadpool-wade-wilson"
+    },
+    {
+      name: "Captain America",
+      values: ["#4992cd", "#d93343"],
+      link: "https://www.marvel.com/characters/captain-america-steve-rogers"
+    },
+    {
+      name: "Spider-Man",
+      values: ["#f45552", "#6f9cf6"],
+      link: "https://www.marvel.com/characters/spider-man-peter-parker"
+    },
+    {
+      name: "Thor",
+      values: ["#ecdc75", "#8c9ed9"],
+      link: "https://www.marvel.com/characters/thor-thor-odinson"
+    },
+    {
+      name: "Hulk",
+      values: ["#7bbc5c", "#836cc1"],
+      link: "https://www.marvel.com/characters/hulk-bruce-banner"
+    },
+    {
+      name: "Black Panther",
+      values: ["#2b2d2f", "#797a7c"],
+      link: "https://www.marvel.com/characters/black-panther-t-challa"
+    }
+  ]
+};
--- src/data/color/index.js
+++ src/data/color/index.js
@@ -2,5 +2,6 @@ import { single } from "./single";
 import { contrast } from "./contrast";
 import { colorHunt } from "./color-hunt";
 import { overwatch } from "./overwatch";
+import { avengers } from "./avengers";
 
-export const colorStore = [single, contrast, colorHunt, overwatch];
+export const colorStore = [single, contrast, colorHunt, overwatch, avengers];
