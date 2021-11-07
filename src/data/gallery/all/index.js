b490b03f9e1237743cdc462373b94f843f2693ca
angelii <527971893@qq.com>
2021-11-08 04:15:52 +0800

    feat(example): add some new examples

--- src/data/gallery/all/index.js
+++ src/data/gallery/all/index.js
@@ -10,6 +10,10 @@ import { mountain } from "./mountain";
 import { bigWorld } from "./big-world";
 import { falling } from "./falling";
 import { challenge } from "./challenge";
+import { beYourself } from "./be-yourself";
+import { iMissYou } from "./i-miss-you";
+import { keepGoodMood } from './keep-good-mood';
+import { niceDay } from './have-a-nice-day';
 
 export const all = {
   name: "all",
@@ -22,6 +26,10 @@ export const all = {
     spring,
     tracer,
     jordan,
+    beYourself,
+    keepGoodMood,
+    iMissYou,
+    niceDay,
     mountain,
     bigWorld,
     falling,
